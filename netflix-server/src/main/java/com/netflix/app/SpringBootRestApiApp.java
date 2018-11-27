30dea79add9d39adb835f6d356cb1b2b171f078e
Vajid Kagdi <vajid.kagdi@sjsu.edu>
2018-11-26 21:22:57 -0800

    CORS issue resolved

--- netflix-server/src/main/java/com/netflix/app/SpringBootRestApiApp.java
+++ netflix-server/src/main/java/com/netflix/app/SpringBootRestApiApp.java
@@ -2,16 +2,31 @@ package com.netflix.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.SecurityAutoConfiguration;
-import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
-
-//@Configuration @EnableAutoConfiguration @ComponentScan
-@SpringBootApplication(scanBasePackages={"com.netflix.app"} ,exclude = { SecurityAutoConfiguration.class }) 
-@EnableJpaAuditing 
+////@EnableJpaAuditing 
+//@Configuration @ComponentScan
+//@SpringBootApplication(scanBasePackages={"com.netflix.app"},exclude = { SecurityAutoConfiguration.class } )
+@SpringBootApplication
 public class SpringBootRestApiApp {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootRestApiApp.class, args);
+	}
+
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurerAdapter() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+            	System.out.println("&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&&");
+
+				registry.addMapping("/**").allowedOrigins("*").allowedMethods("GET", "POST","PUT","OPTIONS",
+						"DELETE").allowCredentials(true);
+			}
+		};
 	}
 }
