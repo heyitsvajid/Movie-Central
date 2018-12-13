ba8d7f3fb8d81a17e5ac4b19ecda85a428404948
MURTAZA <murtazamanasawala110@gmail.com>
2018-12-12 23:04:38 -0800

    Adding db configuration for solr

---
+++ netflix-server/src/main/java/com/netflix/app/controller/DBClient.java
@@ -0,0 +1,18 @@
+package com.netflix.app.controller;
+import org.apache.solr.client.solrj.SolrClient;
+import org.apache.solr.client.solrj.impl.HttpSolrClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@ComponentScan
+
+public class DBClient {
+	@Bean
+	public static SolrClient getConnection() {
+		SolrClient solr = new HttpSolrClient.Builder(/*SolrConfig.BASE_URL*/"http://localhost:8983/solr").build();
+		return solr;
+	}
+}
